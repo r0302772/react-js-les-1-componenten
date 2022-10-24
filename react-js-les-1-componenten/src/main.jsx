ea4f11de35621e226c35f55d23e3d03e96547504
fofokseek <dennis.nijs@hotmail.com>
2022-10-24 09:40:51 +0200

    added jsx exerciseone, header, row, mt, main.css - finished exercise

--- react-js-les-1-componenten/src/main.jsx
+++ react-js-les-1-componenten/src/main.jsx
@@ -1,10 +1,14 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
-import Exercise from "./components/Exercise.jsx";
+import Exercise from "./components/Exercise.jsx"
+import "./main.css"
+import ExerciseOne from "./components/exerciseOne/ExerciseOne.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Exercise title={'Exercise 1: Multiplication Table'}></Exercise>
+        <Exercise title={'Exercise 1: Multiplication Table'}>
+            <ExerciseOne/>
+        </Exercise>
         <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}></Exercise>
         <Exercise title={'Exercise 3: Progressbar'}></Exercise>
         <Exercise title={'Exercise 4: Number grid'} background={'#77EEEE'}></Exercise>
