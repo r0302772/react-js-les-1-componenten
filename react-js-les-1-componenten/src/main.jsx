a41ffb2415c049348f290a6849e2862e44fbf765
fofokseek <dennis.nijs@hotmail.com>
2022-10-24 09:07:07 +0200

    updated main

--- react-js-les-1-componenten/src/main.jsx
+++ react-js-les-1-componenten/src/main.jsx
@@ -4,10 +4,10 @@ import Exercise from "./components/Exercise.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Exercise title={'Exercise 1: Multiplication Table'}/>
-        <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}/>
-        <Exercise title={'Exercise 3: Progressbar'}/>
-        <Exercise title={'Exercise 4: Number grid'} background={'#77EEEE'}/>
-        <Exercise title={'Exercise 5: Comment Card'}/>
+        <Exercise title={'Exercise 1: Multiplication Table'}></Exercise>
+        <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}></Exercise>
+        <Exercise title={'Exercise 3: Progressbar'}></Exercise>
+        <Exercise title={'Exercise 4: Number grid'} background={'#77EEEE'}></Exercise>
+        <Exercise title={'Exercise 5: Comment Card'}></Exercise>
     </StrictMode>
 )
