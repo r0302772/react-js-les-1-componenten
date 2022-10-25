b84ec4acc62f23b748a4868800a4a7a05aad9594
fofokseek <dennis.nijs@hotmail.com>
2022-10-25 11:22:56 +0200

    added exerciseTwo, rater, label, star, extra css, font awesome - finished exercise

--- react-js-les-1-componenten/src/main.jsx
+++ react-js-les-1-componenten/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import Exercise from "./components/Exercise.jsx"
 import "./main.css"
 import ExerciseOne from "./components/exerciseOne/ExerciseOne.jsx";
+import ExerciseTwo from "./components/exerciseTwo/ExerciseTwo.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Exercise title={'Exercise 1: Multiplication Table'}>
             <ExerciseOne/>
         </Exercise>
-        <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}></Exercise>
+        <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}>
+            <ExerciseTwo/>
+        </Exercise>
         <Exercise title={'Exercise 3: Progressbar'}></Exercise>
         <Exercise title={'Exercise 4: Number grid'} background={'#77EEEE'}></Exercise>
         <Exercise title={'Exercise 5: Comment Card'}></Exercise>
