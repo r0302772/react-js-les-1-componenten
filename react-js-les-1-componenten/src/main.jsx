f9ed0937e9bbfa4abc754915c3f467dd7c44d52a
fofokseek <dennis.nijs@hotmail.com>
2022-10-25 11:52:16 +0200

    added excerciseThree, progressbar, label, bar - finished exercise

--- react-js-les-1-componenten/src/main.jsx
+++ react-js-les-1-componenten/src/main.jsx
@@ -4,6 +4,7 @@ import Exercise from "./components/Exercise.jsx"
 import "./main.css"
 import ExerciseOne from "./components/exerciseOne/ExerciseOne.jsx";
 import ExerciseTwo from "./components/exerciseTwo/ExerciseTwo.jsx";
+import ExerciseThree from "./components/exerciseThree/ExerciseThree.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}>
             <ExerciseTwo/>
         </Exercise>
-        <Exercise title={'Exercise 3: Progressbar'}></Exercise>
+        <Exercise title={'Exercise 3: Progressbar'}>
+            <ExerciseThree/>
+        </Exercise>
         <Exercise title={'Exercise 4: Number grid'} background={'#77EEEE'}></Exercise>
         <Exercise title={'Exercise 5: Comment Card'}></Exercise>
     </StrictMode>
